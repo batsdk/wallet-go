8c66540d13169f7c7a46e15cbb37b783540e4721
batsdk <shemilkaweesha@gmail.com>
2025-08-25 20:22:31 +0530

    Chore: Setup Zap and Dotenv

--- go.mod
+++ go.mod
@@ -5,11 +5,15 @@ go 1.24.1
 require github.com/labstack/echo/v4 v4.13.4
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"log"
+	"os"
+	"wallet-api-go-bc/logging"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("WARNING: No .env file found or error loading it: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	logger,_ := zap.NewProduction()
+	defer logger.Sync()
+
+	logging.Logger = logger;
+
 	e := echo.New()
 	e.Use(middleware.Recover())
 
@@ -15,5 +37,5 @@ func main() {
 	// e.POST("/wallets/:id/transactions", handlers.AddTransaction)
 	// e.GET("/wallets/:id/transactions", handlers.ListTransactions)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":"+port))
 }
